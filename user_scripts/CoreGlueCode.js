5166509a22c27dce3ae8b2cf3dfd0a1d7d52ad02
taisel <grantgalitz@gmail.com>
2019-12-04 03:17:45 -0500

    Revert an older commit
    Backout some timing  reclocking changes that were unreliable on some platforms.

--- user_scripts/CoreGlueCode.js
+++ user_scripts/CoreGlueCode.js
@@ -131,11 +131,9 @@ function initTimer() {
     IodineGUI.coreTimerID = setInterval(function () {
         IodineGUI.Iodine.timerCallback(((+(new Date()).getTime()) - (+IodineGUI.startTime)) >>> 0);
     }, IodineGUI.defaults.timerRate | 0);
-	findRealClock();
 }
 function calculateTiming() {
 	IodineGUI.Iodine.setIntervalRate(+IodineGUI.defaults.timerRate);
-	findRealClock();
 }
 function startTimer() {
 	IodineGUI.coreTimerID = setInterval(function () {
@@ -154,81 +152,6 @@ function updateTimer(newRate) {
 			initTimer();
 		}
 	}
-}
-function findRealClock() {
-	var count = 0;
-	var startTime = +(new Date()).getTime();
-	var realTimer = setInterval(function () {
-		if (!IodineGUI.suspended) {
-			count = ((count | 0) + 1) | 0;
-			if (count == 20) {
-				var timeDiff = ((+(new Date()).getTime()) - (+startTime)) >>> 0;
-				var trueRate = timeDiff / 20;
-				if ((+trueRate) > 17) {
-					count = 0;
-				}
-				else {
-					var delta = +((+trueRate) - (IodineGUI.defaults.timerRate | 0));
-					if ((IodineGUI.defaults.timerRate | 0) == 16) {
-						if (delta > -0.1 && delta < 0.1) {
-							clearInterval(realTimer);
-						}
-						else if ((trueRate - 1000/64) > -0.1 && (trueRate - 1000/64) < 0.1) {
-							IodineGUI.Iodine.setIntervalRate(1000/64);
-							clearInterval(realTimer);
-						}
-						else if ((trueRate - 50/3) > -0.1 && (trueRate - 50/3) < 0.1) {
-							IodineGUI.Iodine.setIntervalRate(50/3);
-							clearInterval(realTimer);
-						}
-						else {
-							count = 0;
-						}
-					}
-					else if ((IodineGUI.defaults.timerRate | 0) == 4) {
-						if (delta > -0.1 && delta < 0.1) {
-							clearInterval(realTimer);
-						}
-						else if ((trueRate - 25/6) > -0.1 && (trueRate - 25/6) < 0.1) {
-							IodineGUI.Iodine.setIntervalRate(25/6);
-							clearInterval(realTimer);
-						}
-						else {
-							count = 0;
-						}
-					}
-					else if ((IodineGUI.defaults.timerRate | 0) == 8) {
-						if (delta > -0.1 && delta < 0.1) {
-							clearInterval(realTimer);
-						}
-						else if ((trueRate - 25/3) > -0.1 && (trueRate - 25/3) < 0.1) {
-							IodineGUI.Iodine.setIntervalRate(25/3);
-							clearInterval(realTimer);
-						}
-						else {
-							count = 0;
-						}
-					}
-					else {
-						if (delta > -0.1 && delta < 0.1) {
-							clearInterval(realTimer);
-						}
-						else if (trueRate < 16) {
-							IodineGUI.Iodine.setIntervalRate(+trueRate);
-							clearInterval(realTimer);
-						}
-						else {
-							count = 0;
-						}
-					}
-					
-				}
-			}
-		}
-		else {
-			count = 0;
-		}
-    }, IodineGUI.defaults.timerRate | 0);
 }
 function registerBlitterHandler() {
     IodineGUI.Blitter = new GfxGlueCode(240, 160);
